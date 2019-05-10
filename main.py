21a379432fc305eca905aac1c0a287b203723c5c
Daniel Filipe Nunes Silva <thenitronos@gmail.com>
2019-05-10 17:59:03 +0200

    Implement read and return np array of bytes

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import argparse
 import pathlib
 
+import numpy as np
+
 TEXT_FILES_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Text-Files/")
 
 if __name__ == '__main__':
@@ -14,8 +16,8 @@ if __name__ == '__main__':
                         help='.txt file containing chararacters.')
 
     args = parser.parse_args()
-
     args.input_file = pathlib.Path(TEXT_FILES_PATH+args.input_file).resolve(strict=True)
+
     if not (args.input_file.is_file() and
             (args.input_file.suffix == '.txt')):
         raise ValueError('Parameter[input_file] is not a .txt file.')
--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,16 @@
+import numpy as np
+
 def read_file(filename):
-    text = None
+    read_chars_in_bits = None
 
-    with open(filename, mode="rt", encoding="utf-8") as file:
-        text = file.read()
+    with open(filename, mode="rb") as file:
+        read_chars_in_bits = file.read()
 
-    return text
+    read_chars_in_bits_length = len(read_chars_in_bits)
+
+    np_array_of_bytes = np.empty(read_chars_in_bits_length, dtype=np.int8)
+
+    for i in range(read_chars_in_bits_length):
+        np_array_of_bytes[i] = read_chars_in_bits[i]
+
+    return np_array_of_bytes
